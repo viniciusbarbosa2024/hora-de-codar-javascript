4c95af799083ac1907ea89f4e42feee9eece6348
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-08-20 20:22:20 -0300

    Mais sobre objetos
    Adicionar/remover propriedades + método entries

---
+++ arrays-e-objetos/adicionar-e-remover-propriedades.js
@@ -0,0 +1,10 @@
+const teste = {
+    a: 1,
+    b:'abc'
+}
+
+teste.c = true //Adiciona a propriedade c no objeto
+
+delete(teste.a) //Deleta a propriedade a
+
+console.log(teste)
---
+++ arrays-e-objetos/metodo-entries.js
@@ -0,0 +1,7 @@
+const teste = {
+    a: 50,
+    b: 'dkghsdkg',
+    c: false
+}
+
+console.log(Object.entries(teste)) //O Object.entries retorna um array composto por arrays,tendo estes 2 elementos sendo o primeiro o nome da propriedade e o segundo o valor da propriedade
