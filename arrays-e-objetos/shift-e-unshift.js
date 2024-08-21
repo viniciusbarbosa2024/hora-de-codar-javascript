322ea12a8446108eb3e31b1dbb3073fef7d12818
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-08-21 13:22:38 -0300

    shift e unshift

---
+++ arrays-e-objetos/shift-e-unshift.js
@@ -0,0 +1,15 @@
+//Shift remove o primeiro elemento do array (Funciona como o pop,só que no início)
+
+let teste = ['a','b','c']
+
+let itemRemovido = teste.shift()
+
+console.log(itemRemovido)
+
+console.log(teste)
+
+//Unshift funciona como o push,só que no início do array 
+
+teste.unshift('x','y')
+
+console.log(teste)
