dafe508635e6b8a98d8e64c009bc084871542855
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-08-28 13:23:10 -0300

    destructuring em arrays

---
+++ arrays-e-objetos/destructuring-em-arrays.js
@@ -0,0 +1,9 @@
+//Segue sintaxe semelhante ao destructuring de objetos
+
+const teste = [1,2,3,4,5,6,7,8,9]
+
+const [n1,n2] = teste //Como o array não possui propriedades (como os objetos) n1 e n2 serão necessariamente o primeiro e o segundo elemento do array, respectivamente.
+
+console.log(n1)
+
+console.log(n2)
